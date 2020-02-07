7b8706ad20a3dc258c49e3164824a9fba5a88c5a
Martin Splitt <splitti@google.com>
2020-02-07 10:52:00 +0100

    Update script.js

--- web-workers/script.js
+++ web-workers/script.js
@@ -3,8 +3,8 @@ var list = document.querySelector('ul');
 if (window.Worker) {
   var worker = new Worker('worker.js?cachebust=' + new Date());  
   worker.addEventListener('message', function handleMsg(evt) {
-    console.log('[Main] Image received. ' + evt.data.url);
     if (evt.data.type === 'img') {
+      console.log('[Main] Image received. URL to load: ' + evt.data.url);
       appendImage(evt.data.url, list);      
     } else {
       console.log('Received from worker: ', evt.data);
