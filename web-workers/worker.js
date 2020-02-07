afe95245b4da7669e62628fb80fc70d3b9e527b3
Martin Splitt <splitti@google.com>
2020-02-07 11:23:55 +0100

    Better logging

--- web-workers/worker.js
+++ web-workers/worker.js
@@ -5,13 +5,13 @@ self.onmessage = function(evt) {
   waitMS = evt.data.delay || 1000;
   self.postMessage({type: 'echo', msg: evt.data});
   // load more cats!
+  console.log('[Worker] Async load - using delay ' + waitMS);
   for(var i=0; i<5;i++) {
     (function(index) {
       setTimeout(function() {
         console.log('[Worker] Sent img #' + (20 + index));
         self.postMessage({ type: 'img', url: 'https://cat-api-237122.appspot.com/img/' + (index + 20) + '.jpg'});     
       }, index * waitMS);
-      console.log('Async - using delay ' + waitMS);
     })(i);
   }
   
@@ -19,6 +19,7 @@ self.onmessage = function(evt) {
 
 // begin the actual work: Send URLs of cat pictures to the main thread
 // the first one is sent immediately
+console.log('[Worker] Sync load - using delay ' + waitMS);
 self.postMessage({ type: 'img', url: 'https://cat-api-237122.appspot.com/img/13.jpg'});     
 for(var i=0; i<5;i++) {
   (function(index) {
@@ -26,6 +27,5 @@ for(var i=0; i<5;i++) {
       console.log('[Worker] Sent img #' + index);
       self.postMessage({ type: 'img', url: 'https://cat-api-237122.appspot.com/img/' + (index + 1) + '.jpg'});     
     }, index * waitMS);
-    console.log('Sync - using delay ' + waitMS);
   })(i);
 }
